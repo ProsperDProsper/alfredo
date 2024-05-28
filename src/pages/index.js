c89393a3b34e68fe9f1081c914e1e2097a58996e
prosperdeogratius <prosperdeogratius@gmail.com>
2024-05-28 13:48:16 +0000

    removed framer motion temporalily

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,14 @@ import Products from "@/components/sections/home/Products";
 import React from "react";
 import { motion } from "framer-motion";
 import { staggerContainer } from "@/utils/motion";
-import FashionQuote from "@/components/sections/home/FashionQuote";
 import Footer from "@/components/Footer";
 export default function Home() {
 	return (
 		<motion.div
-			variants={staggerContainer}
-			initial="hidden"
-			whileInView="show"
-			viewport={{ once: true, amount: 0.25 }}
+			// variants={staggerContainer}
+			// initial="hidden"
+			// whileInView="show"
+			// viewport={{ once: false, amount: 0.25 }}
 			className="overflow-hidden"
 		>
 			<main>
